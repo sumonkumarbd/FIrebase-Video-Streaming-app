d5d6dd74c222ae2789627838f926ecbab3e6ca21
sumonkmr <sumonkmr.dev@gmail.com>
2022-02-07 13:18:05 +0600

    Here I've implemented full step of this fileUploading in firebase Storage and realtime database system!!

--- app/src/main/java/com/sumonkmr/sharingmomments/AddVideos.java
+++ app/src/main/java/com/sumonkmr/sharingmomments/AddVideos.java
@@ -1,16 +1,29 @@
 package com.sumonkmr.sharingmomments;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -24,15 +37,23 @@ public class AddVideos extends AppCompatActivity {
     Button browse,upload;
     Uri videoUri;
     MediaController mediaController;
+    EditText vtitle;
+    StorageReference storageReference;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_videos);
 
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference("videos");
+
         videoView = findViewById(R.id.videoView);
         browse = findViewById(R.id.browse);
         upload = findViewById(R.id.upload);
+        vtitle = findViewById(R.id.vtitle);
         mediaController = new MediaController(this);
         videoView.setMediaController(mediaController);
         videoView.start();
@@ -62,8 +83,47 @@ public class AddVideos extends AppCompatActivity {
                     }).check();
         });
 
+        upload.setOnClickListener(view -> {
+            proccesVideoUploading();
+        });
 
 
+
+    }
+
+    public String GetExtention(){
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(getContentResolver().getType(videoUri));
+    }
+
+    private void proccesVideoUploading() {
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Media Uploader");
+        progressDialog.show();
+
+        StorageReference uploader = storageReference.child("videos/"+System.currentTimeMillis()+"."+GetExtention());
+        uploader.putFile(videoUri)
+                .addOnSuccessListener(taskSnapshot -> {
+                    uploader.getDownloadUrl().addOnSuccessListener(uri -> {
+                        ModelClass modelClass = new ModelClass(vtitle.getText().toString(),uri.toString());
+                        databaseReference.child(databaseReference.push().getKey()).setValue(modelClass)
+                                .addOnSuccessListener(unused -> {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(getApplicationContext(), "Successfully Uploaded!!", Toast.LENGTH_SHORT).show();
+                                })
+                                .addOnFailureListener(e -> {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(getApplicationContext(), "Failed to Upload!!", Toast.LENGTH_SHORT).show();
+                                });
+                    });
+                })
+
+
+                .addOnProgressListener(snapshot -> {
+                    float persentage = (100*snapshot.getBytesTransferred())/snapshot.getTotalByteCount();
+                    progressDialog.setMessage("Uploading: " + (int)persentage + " %");
+                });
     }
 
     @Override
